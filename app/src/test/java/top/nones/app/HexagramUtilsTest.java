c2b43ebeb355f8ce0138ebf0832087198f119d01
myphone <yywolf1983@gmail.com>
2025-01-25 12:21:35 +0800

    add UtilsTest

--- app/src/test/java/top/nones/app/HexagramUtilsTest.java
+++ app/src/test/java/top/nones/app/HexagramUtilsTest.java
@@ -1,17 +1,20 @@
+package top.nones.app;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class HexagramUtilsTest {
     @Test
-    public void testQianHexagram() {
-        // 测试乾卦
+    public void testGenerateHexagramLines() {
         int[] lines = HexagramUtils.generateHexagramLines(0);
         assertEquals("111111", HexagramUtils.getHexagramBinary(0));
         assertTrue(HexagramUtils.validateHexagram(0, "111111"));
     }
 
     @Test
-    public void testKunHexagram() {
-        // 测试坤卦
+    public void testGenerateHexagramLines2() {
         int[] lines = HexagramUtils.generateHexagramLines(1);
         assertEquals("000000", HexagramUtils.getHexagramBinary(1));
         assertTrue(HexagramUtils.validateHexagram(1, "000000"));
     }
-} 
+}
